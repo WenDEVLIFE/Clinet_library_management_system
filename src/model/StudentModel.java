97f2ebb97c8cae614dc832c3c15bfc29abfb84a8
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-04-21 13:52:46 +0800

    Added StudentModel 👌

---
+++ src/model/StudentModel.java
@@ -0,0 +1,71 @@
+package model;
+
+public class StudentModel {
+
+    final String userid;
+    final String username;
+    final String password;
+    final String yearAndSection;
+    final String homeAddress;
+    final String emailAddress;
+    final String studentNumber;
+    final String phoneNumber;
+
+
+    public StudentModel(String userid, String username, String password, String yearAndSection, String homeAddress, String emailAddress, String studentNumber, String phoneNumber) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.yearAndSection = yearAndSection;
+        this.homeAddress = homeAddress;
+        this.emailAddress = emailAddress;
+        this.studentNumber = studentNumber;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getYearAndSection() {
+        return yearAndSection;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "userid='" + userid + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", yearAndSection='" + yearAndSection + '\'' +
+                ", homeAddress='" + homeAddress + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", studentNumber='" + studentNumber + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
